397545aabe4836f2e1c04bc9daa5387a1e98cb3c
Philippe DELEFOSSE <pdelefos@e2r3p21.42.fr>
2018-03-15 18:47:37 +0100

    converted to module pattern

--- main.js
+++ main.js
@@ -1,76 +1,67 @@
 // Mouse parallax lib
-const parallaxBox = document.querySelector(".parallax-box")
-const layer1Top = document.getElementById("layer-1").offsetTop
-const layer1Left = document.getElementById("layer-1").offsetLeft
-const layer2Top = document.getElementById("layer-2").offsetTop
-const layer2Left = document.getElementById("layer-2").offsetLeft
-const layer3Top = document.getElementById("layer-3").offsetTop
-const layer3Left = document.getElementById("layer-3").offsetLeft
+var Paralalax = (function() {
+  let parallaxBox = null
+  let layers = []
+  let speed = 0
 
-parallaxBox.onmousemove = function(evt) {
-  // get mouse position related to the parallax box
-  const x = evt.clientX - parallaxBox.offsetLeft
-  const y = evt.clientY - parallaxBox.offsetTop
-
-  mouseParallax("layer-1", layer1Left, layer1Top, x, y, 40)
-  mouseParallax("layer-2", layer2Left, layer2Top, x, y, 80)
-  mouseParallax("layer-3", layer3Left, layer3Top, x, y, 150)
-}
-
-function mouseParallax(
-  layerId,
-  layerOffsetLeft,
-  layerOffsetTop,
-  mousePosX,
-  mousePosY,
-  speed
-) {
-  const layer = document.getElementById(layerId)
-  const layerParent = layer.parentNode
-  parallaxBoxWidth = layerParent.offsetWidth
-  parallaxBoxHeight = layerParent.offsetHeight
-  const newPosLeft =
-    layerOffsetLeft -
-    (mousePosX - parseInt(layer.offsetWidth) / 2 + layerOffsetLeft) /
-      parallaxBoxWidth *
-      speed
-  const newPosTop =
-    layerOffsetTop -
-    (mousePosY - parseInt(layer.offsetHeight) / 2 + layerOffsetTop) /
-      parallaxBoxHeight *
-      speed
-  layer.style.transform =
-    "translate3d(" + newPosLeft + "px," + newPosTop + "px, 0)"
-}
-// Audio context creation
-let audioContext = null
-window.addEventListener("load", evt => {
-  try {
-    window.AudioContext = window.AudioContext || window.webkitAudioContext
-    audioContext = new AudioContext()
-    console.log(audioContext)
-  } catch (e) {
-    console.warn("Web Audio API is not supported in this browser")
+  var doParallax = function(
+    layerElem,
+    layerOffsetLeft,
+    layerOffsetTop,
+    mousePosX,
+    mousePosY,
+    speed
+  ) {
+    const layer = layerElem
+    const layerParent = layer.parentNode
+    parallaxBoxWidth = layerParent.offsetWidth
+    parallaxBoxHeight = layerParent.offsetHeight
+    const newPosLeft =
+      layerOffsetLeft -
+      (mousePosX - parseInt(layer.offsetWidth) / 2 + layerOffsetLeft) /
+        parallaxBoxWidth *
+        speed
+    const newPosTop =
+      layerOffsetTop -
+      (mousePosY - parseInt(layer.offsetHeight) / 2 + layerOffsetTop) /
+        parallaxBoxHeight *
+        speed
+    layer.style.transform =
+      "translate3d(" + newPosLeft + "px," + newPosTop + "px, 0)"
   }
-  const button = document.querySelector(".square-3")
-  button.onclick = () => {
-    let soundBuffer = null
-    loadSound(
-      "https://ia600607.us.archive.org/15/items/Spacejunk_201707/Spacejunk.flac"
+  var parallaxEvent = function(evt) {
+    // get mouse position related to the parallax box
+    const x = evt.clientX - parallaxBox.offsetLeft
+    const y = evt.clientY - parallaxBox.offsetTop
+
+    layers.forEach(layer =>
+      doParallax(
+        layer.elem,
+        layer.offsetLeft,
+        layer.offsetTop,
+        x,
+        y,
+        layer.speed
+      )
     )
   }
-})
-// Loading audio file
-function loadSound(url) {
-  soundBuffer = audioContext.createBufferSource()
-  fetch(url, { method: "GET" })
-    .then(response => response.arrayBuffer())
-    .then(buffer => {
-      audioContext.decodeAudioData(buffer, decodedData => {
-        soundBuffer.buffer = decodedData
-        soundBuffer.connect(audioContext.destination)
-        soundBuffer.start(0)
+  var init = function(options) {
+    parallaxBox = document.querySelector(".parallax-box")
+    speed = options.speed
+    const layersArray = parallaxBox.querySelectorAll(".layer")
+
+    layersArray.forEach(layer => {
+      layers.push({
+        elem: layer,
+        offsetTop: layer.offsetTop,
+        offsetLeft: layer.offsetLeft,
+        speed: speed
       })
+      speed *= 2
     })
-    .catch(err => console.error(err))
-}
+    parallaxBox.onmousemove = parallaxEvent
+  }
+  return { init }
+})()
+
+Paralalax.init({ speed: 10 })
